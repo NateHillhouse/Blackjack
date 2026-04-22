155f02838c02e97b18ac045b114a8288ae0af262
mizterwaffles <nhillhouse04@gmail.com>
2026-04-22 12:35:40 -0600

    Added Card Struct

---
+++ Classes/Cards.cs
@@ -0,0 +1,24 @@
+﻿namespace Classes;
+
+public readonly struct Cards(Suite suite, int number)
+{
+    private readonly Suite CardSuite { get; } = suite;
+    public readonly Suite GetSuite => CardSuite;
+    private readonly int Value { get; } = number;
+    public readonly int CardValue => Value;
+
+    public readonly Color CardColor
+    {
+        get => CardSuite switch
+        {
+            Suite.Clubs => Color.Black,
+            Suite.Spades => Color.Black,
+            Suite.Hearts => Color.Red,
+            Suite.Diamonds => Color.Red,
+            _ => Color.Blank
+        };
+    }
+}
+
+public enum Suite {Spades, Diamonds, Clubs, Hearts}
+public enum Color {Red, Black, Blank}
